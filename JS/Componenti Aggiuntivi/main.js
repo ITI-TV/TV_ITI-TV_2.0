581dde3373b1d895555465b3dddf8e9b836f32f3
ZELA2000 <79649375+ZELA2000@users.noreply.github.com>
2024-08-27 13:21:14 +0200

    Conclusione Grafica Halloween (Componenti Aggiuntivi & Eventi Giornalieri)

--- JS/Componenti Aggiuntivi/main.js
+++ JS/Componenti Aggiuntivi/main.js
@@ -33,4 +33,37 @@ function load(data) {
     document.getElementById("ProfComponentiAggiuntivi").innerHTML = componenteAggiuntivo.Prof;
 }
 
-startComponentiAggiuntivi();
+function startComponentiAggiuntiviHall() {
+    document.body.style.backgroundColor = "#0c0622";
+    checkServer();
+    fetch("PHP/getters.php?action=getComponentiAggiuntivi")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Errore HTTP: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            loadHall(data);
+        })
+        .catch(error => {
+            console.error('Errore nella richiesta:', error);
+        });
+}
+
+function loadHall(data) {
+    //scielgo un numero a caso tra 0 e la dimensione dell'array
+    var numeroCasuale = Math.floor(Math.random() * data.length);
+    //prendo il componente aggiuntivo
+    var componenteAggiuntivo = data[numeroCasuale];
+    if (componenteAggiuntivo == null) {
+        document.getElementById("TitoloComponentiAggiuntiviHall").innerHTML = "Nessun componente aggiuntivo disponibile";
+        document.getElementById("TestoComponentiAggiuntiviHall").innerHTML = "Nessun componente aggiuntivo disponibile";
+        document.getElementById("ProfComponentiAggiuntiviHall").innerHTML = "ITI-TV";
+        return;
+    }
+    document.getElementById("TitoloComponentiAggiuntiviHall").innerHTML = componenteAggiuntivo.Titolo;
+    document.getElementById("TestoComponentiAggiuntiviHall").innerHTML = componenteAggiuntivo.Testo;
+    document.getElementById("ImmagineComponentiAggiuntiviHall").src = componenteAggiuntivo.Immagine;
+    document.getElementById("ProfComponentiAggiuntiviHall").innerHTML = componenteAggiuntivo.Prof;
+}
--- JS/Eventi Giornalieri/main.js
+++ JS/Eventi Giornalieri/main.js
@@ -41,4 +41,45 @@ function load(data) {
     document.getElementById("ImmagineEventiGiornalieri").src = eventoGiornaliero.Immagine;
 }
 
-startEventiGiornalieri();
+function startEventiGiornalieriHall() {
+    document.body.style.backgroundColor = "#0c0622";
+    let data = new Date();
+    let giorno = data.getDate();
+    let mese = data.getMonth() + 1;
+    if (giorno < 10) {
+        giorno = "0" + giorno;
+    }
+    if (mese < 10) {
+        mese = "0" + mese;
+    }
+    document.getElementById("giornoAttualeEventiGiornalieriHall").innerHTML = "Oggi " + giorno + "/" + mese;
+    checkServer();
+    fetch("PHP/getters.php?action=getEventiGiornalieri")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Errore HTTP: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            loadHall(data);
+        })
+        .catch(error => {
+            console.error('Errore nella richiesta:', error);
+        });
+}
+
+function loadHall(data) {
+    //scielgo un numero a caso tra 0 e la dimensione dell'array
+    var numeroCasuale = Math.floor(Math.random() * data.length);
+    //prendo il componente aggiuntivo
+    var eventoGiornaliero = data[numeroCasuale];
+    if (eventoGiornaliero == null) {
+        document.getElementById("TitoloEventiGiornalieriHall").innerHTML = "Nessun evento giornaliero disponibile";
+        document.getElementById("TestoEventiGiornalieriHall").innerHTML = "Nessun evento giornaliero disponibile";
+        return;
+    }
+    document.getElementById("TitoloEventiGiornalieriHall").innerHTML = eventoGiornaliero.Titolo;
+    document.getElementById("TestoEventiGiornalieriHall").innerHTML = eventoGiornaliero.Testo;
+    document.getElementById("ImmagineEventiGiornalieriHall").src = eventoGiornaliero.Immagine;
+}
